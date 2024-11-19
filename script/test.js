dd0e8015467c91cf45d5d937649a93467029f052
Deuteronome <o.burcker@e2c-grandlille.fr>
2024-11-19 15:29:06 +0100

    début JS

---
+++ script/test.js
@@ -0,0 +1,12 @@
+
+//console.log("Hello World!");
+
+//let userName = "Mister Bear"; //string
+//let age = 22; //int
+//let size = 1.95; //float
+//let isCool = true; //bool
+
+let a = 15
+let b = 35
+
+console.log(a+b, a/b, a-b, a*b, a%b)
